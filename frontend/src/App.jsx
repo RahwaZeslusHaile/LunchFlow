7096ef486dda304335b1f980347315139b79ed26
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-09 19:44:25 +0000

    Make App.jsx responsible for rendering the homepage ONLY

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,7 @@
+import LandingPage from "./LandingPage";
+
 function App() {
-  return (
-    <main className="app">
-      <h1>🍱 CYF Lunch Organiser</h1>
-      <p>Manage weekly lunch orders for CYF London classes</p>
-      <div className="homepage-buttons">
-        <button>Sign up</button>
-        <button>Log in</button>
-      </div>
-      <p>Access is limited to authorised CYF volunteers.</p>
-    </main>
-  );
+  return <LandingPage/>;
 }
 
 export default App;
