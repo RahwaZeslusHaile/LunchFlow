306a11ec762ff9ab27cf96cc095acba21deab4dd
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-10 22:01:39 +0000

    Make title bold and bigger

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,7 +1,7 @@
 function LandingPage() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50 gap-6 p-4">
-      <h1>🍱 CYF Lunch Organiser</h1>
+      <h1 className="text-4xl font-bold">🍱 CYF Lunch Organiser</h1>
       <p>Manage weekly lunch orders for CYF London classes</p>
       <div className="homepage-buttons">
         <button>Sign up</button>
