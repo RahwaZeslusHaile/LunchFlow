2ef632240e7566222dfa01f1c2871efb7a2f631b
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-10 21:17:19 +0000

    tailwind setup/ config

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,6 +1,6 @@
 function LandingPage() {
   return (
-    <main className="app">
+    <main className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <h1>🍱 CYF Lunch Organiser</h1>
       <p>Manage weekly lunch orders for CYF London classes</p>
       <div className="homepage-buttons">
