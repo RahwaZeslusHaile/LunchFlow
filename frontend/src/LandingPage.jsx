4d1daee926ace99cb2c5f5dbff3a82174fdc0ee9
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-10 22:06:37 +0000

    Add div tag to act as card container that hold all page content

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,13 +1,17 @@
 function LandingPage() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50 gap-6 p-4">
-      <h1 className="text-4xl font-bold">🍱 CYF Lunch Organiser</h1>
-      <p>Manage weekly lunch orders for CYF London classes</p>
-      <div className="homepage-buttons">
-        <button>Sign up</button>
-        <button>Log in</button>
+      <div className="max-w-md p-8 bg-white border rounded-xl shadow-md space-y-6 text-center">
+        <h1 className="text-4xl font-bold">🍱 CYF Lunch Organiser</h1>
+        <p className="text-gray-600 text-center max-w-md">
+          Manage weekly lunch orders for CYF London classes
+        </p>
+        <div className="homepage-buttons">
+          <button>Sign up</button>
+          <button>Log in</button>
+        </div>
+        <p>Access is limited to authorised CYF volunteers.</p>
       </div>
-      <p>Access is limited to authorised CYF volunteers.</p>
     </main>
   );
 }
