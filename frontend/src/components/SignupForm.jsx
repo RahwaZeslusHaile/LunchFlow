6dcf1ba84742af539e03470fe287b196e1a0e38c
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-11 13:08:34 +0000

    update/add more attributes for email, password, and confirm pasword input fields so react can track changes on them

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -8,9 +8,9 @@ function SignupForm() {
     return (
       <form>
         <h2>Sign Up</h2>
-        <input type="email" placeholder="Email" required></input>
-        <input type="password" placeholder="Password" required></input>
-        <input type="password" placeholder="Confirm Password" required></input>
+        <input type="email" value={userName} placeholder="Email" onChange={(e) => setUserName(e.target.value)} required></input>
+        <input type="password" value={password} placeholder="Password" onChange={(e) => setPasswrd(e.target.value)} required></input>
+        <input type="password" value={confirmPassword} placeholder="Confirm Password" onChange={(e) => setConfirmPasswrd(e.target.value)} required></input>
         <button type="submit">Sign Up</button>
       </form>
     );
