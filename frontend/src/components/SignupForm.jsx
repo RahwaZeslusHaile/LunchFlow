856037f05e7b6119d6e4210dfa3ee52f18fd6959
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-11 12:54:37 +0000

    add "password" and "confirm password" input boxed

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -1,10 +1,13 @@
-function SignupFrom() {
+function SignupForm() {
     return (
-        <form>
-            <h2>Sign Up</h2>
-            <input type="email" placeholder="Email" required></input>
-        </form>
-
-    )
+      <form>
+        <h2>Sign Up</h2>
+        <input type="email" placeholder="Email" required></input>
+        <input type="password" placeholder="Password" required></input>
+        <input type="password" placeholder="Confirm Password" required></input>
+      </form>
+    );
 
 }
+
+export default SignupFrom;
