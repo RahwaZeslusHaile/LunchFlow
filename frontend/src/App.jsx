cd8a1bb9ae595da0b36a437481ecc728ff671a71
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-11 16:29:44 +0000

    importing SignupFrom, Login, and adding routes to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage";
+import SignupForm from "./components/SignupForm";
+import LoginForm from "./components/LoginForm";
 
 function App() {
-  return <LandingPage/>;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<SignupForm />} />
+      <Route path="/login" element={<LoginForm />} />
+    </Routes>
+  );
 }
 
 export default App;
