a5f6b20368f15eecc2e888f5f914cec55d9c346a
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-12 18:40:43 +0000

    Update  error handing from API response to data.message instead for clearer and safer handling

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+function LoginForm() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!userName.trim()) {
+      setError("Username is required");
+      return;
+    }
+
+    if (!password.trim()) {
+      setError("Password is required");
+      return;
+    }
+
+    console.log("Sending login request to backend:", { userName, password});
+
+    setError("");
+
+    try {
+      const response = await fetch(
+        "http://localhost:4000/api/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ userName, password}),
+        });
+
+        const data = await response.json();
+        console.log("Response from backend:", data);
+
+        if (!response.ok) {
+          setError(data.message);
+          return;
+        }
+
+        alert("login successful");
+
+    } catch (err) {
+      console.error("Server-side or Network error:", err);
+      setError("Server-side error");
+
+    }
+
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Log In</h2>
+      <input
+        type="email"
+        value={userName}
+        placeholder="Email"
+        onChange={(e) => setUserName(e.target.value)}
+        required
+      ></input>
+      <input
+        type="password"
+        value={password}
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      ></input>
+      {error && <p className="text-red-500">{error}</p>}
+      <button type="submit">Log In</button>
+    </form>
+  );
+}
+
+export default LoginForm;
---
+++ frontend/src/components/SignupForm.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+function SignupForm() {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPass, setConfirmPass] = useState("");
+
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!userName.trim()) {
+            setError("Username is required");
+            return;
+        }
+
+        if (!password.trim()) {
+          setError("Password is required");
+          return;
+        }
+
+        if (password !== confirmPass) {
+          setError("Passwords do not match");
+          return;
+        }
+
+        console.log("Sending signup request to backend:", { userName, password, confirmPass });
+
+        setError("");
+
+        try {
+          const response = await fetch(
+            "http://localhost:4000/api/auth/signup",
+            {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ userName, password, confirmPass }),
+            });
+
+            const data = await response.json();
+            console.log("Response from backend:", data);
+
+            if (!response.ok) {
+              setError(data.message);
+              return;
+            }
+
+            alert("Signup successful");
+
+        } catch (err) {
+          console.error("Server-side or Network error:", err);
+          setError("Server-side error");
+
+        }
+
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <input type="email" value={userName} placeholder="Email" onChange={(e) => setUserName(e.target.value)} required></input>
+        <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} required></input>
+        <input type="password" value={confirmPass} placeholder="Confirm Password" onChange={(e) => setConfirmPass(e.target.value)} required></input>
+        {error && <p className="text-red-500">{error}</p>}
+        <button type="submit">Sign Up</button>
+      </form>
+    );
+
+}
+
+export default SignupForm;
