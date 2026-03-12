f9de32412b1b016cb2180e1f4e3a5805ad3f60a8
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-12 19:20:21 +0000

    Style sign up form, in-line with the basic landing page styling - Also tailor stying for user input, ensuring the have borders, placeholders etc

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -57,14 +57,18 @@ function SignupForm() {
     };
 
     return (
-      <form onSubmit={handleSubmit}>
-        <h2>Sign Up</h2>
-        <input type="email" value={userName} placeholder="Email" onChange={(e) => setUserName(e.target.value)} required></input>
-        <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} required></input>
-        <input type="password" value={confirmPass} placeholder="Confirm Password" onChange={(e) => setConfirmPass(e.target.value)} required></input>
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-200 p-4">
+        <form onSubmit={handleSubmit} className="max-w-md w-full p-8 bg-white border border-gray-200 rounded-xl shadow-md space-y-4 text-center">
+        <h2 className="text-3xl font-bold">Sign Up</h2>
+        <input type="email" value={userName} placeholder="Email" onChange={(e) => setUserName(e.target.value)} required className="w-full p-2 border border-gray-300 rounded-lg"></input>
+        <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} required className="w-full p-2 border border-gray-300 rounded-lg"></input>
+        <input type="password" value={confirmPass} placeholder="Confirm Password" onChange={(e) => setConfirmPass(e.target.value)} required className="w-full p-2 border border-gray-300 rounded-lg"></input>
         {error && <p className="text-red-500">{error}</p>}
-        <button type="submit">Sign Up</button>
+        <button type="submit" className="w-full px-6 py-2 border rounded-lg shadow-sm hover:bg-gray-100 hover:shadow transition">Sign Up</button>
       </form>
+        
+      </main>
+ 
     );
 
 }
