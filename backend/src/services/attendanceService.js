84425eaea504012bdf4df6a924f4a75d94cb8a60
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-23 16:19:37 +0000

    adding missing "FROM" to validate SQL

--- backend/src/services/attendanceService.js
+++ backend/src/services/attendanceService.js
@@ -1,2 +1,8 @@
 import pool from "../db.js";
 
+export async function fetchAttendance() {
+  const storedAttendanceData = await pool.query(
+    "SELECT * FROM attendance ORDER BY attendance_id"
+  );
+  return storedAttendanceData.rows;
+}
