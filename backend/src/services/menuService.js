f7fce80720868db58a2aaf7241aeace57ebf89b2
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-23 13:25:01 +0000

    Create function to add new menu item into DB. Export it into menuControllers.js to be used as helper function for its respective controller.

--- backend/src/services/menuService.js
+++ backend/src/services/menuService.js
@@ -24,3 +24,11 @@ export async function fetchMenuItems() {
   `);
   return dbStoredMenuItems.rows;
 }
+
+export async function createMenuItem(name, category_id, diet_id) {
+  const addNewMenuItem = await pool.query(
+    "INSERT INTO menu_items (name, category_id, diet_id) VALUES ($1, $2, $3) RETURNING *",
+    [name, category_id, diet_id]
+  );
+  return addNewMenuItem.rows[0];
+}
