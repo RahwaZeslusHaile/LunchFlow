a7376835f8863d75d8556ad5ea82877634db9598
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-23 16:23:05 +0000

    Join with attendance with classes so that classes names are obtained too

--- backend/src/services/attendanceService.js
+++ backend/src/services/attendanceService.js
@@ -1,8 +1,16 @@
 import pool from "../db.js";
 
 export async function fetchAttendance() {
-  const storedAttendanceData = await pool.query(
-    "SELECT * FROM attendance ORDER BY attendance_id"
-  );
+  const storedAttendanceData = await pool.query(`
+  SELECT
+    a.attendance_id,
+    c.name AS class_name,
+    a.session_date,
+    a.trainee_count,
+    a.volunteer_count
+  FROM attendance a
+  JOIN classes c ON a.class_id = c.class_id
+  ORDER BY a.attendance_id  
+  `);
   return storedAttendanceData.rows;
 }
