305efc7bebe03c4e002bdc220ddbd0163e52051d
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-23 13:12:52 +0000

    creat (and then export) helper function that interact with DB and feteches exisiting categories

--- backend/src/services/menuService.js
+++ backend/src/services/menuService.js
@@ -1,2 +1,6 @@
 import pool from "../db.js";
 
+export async function fetchCategories() {
+   const dbStoredCategories = await pool.query("SELECT * FROM menu_categories ORDER BY category_id");
+   return dbStoredCategories.rows;
+}
