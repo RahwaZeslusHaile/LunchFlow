533e4467e08a43ccd8958cf530977d1dfb8642c8
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-23 11:54:04 +0000

    Add an error handler fucntion

--- backend/src/controllers/menuController.js
+++ backend/src/controllers/menuController.js
@@ -6,4 +6,10 @@ import {
   getDietaryRestrictions,
 } from "../services/menuService.js";
 
-
+function sendError(res, err) {
+  console.error(err);
+  if (err.status) {
+    return res.status(err.status).json(err.message);
+  }
+  return res.status(500).json("Database error");
+}
