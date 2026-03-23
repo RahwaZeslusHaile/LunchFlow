12c618a0ee4770f8d6a7b06bc34ed32263582311
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-23 13:16:35 +0000

    Create addCategory which adds new category to DB. Export it to be used later in menuControllers.js

--- backend/src/services/menuService.js
+++ backend/src/services/menuService.js
@@ -4,3 +4,11 @@ export async function fetchCategories() {
    const dbStoredCategories = await pool.query("SELECT * FROM menu_categories ORDER BY category_id");
    return dbStoredCategories.rows;
 }
+
+export async function addCategory(name) {
+  const addNewDBCategory = await pool.query(
+    "INSERT INTO menu_categories (name) VALUES ($1) RETURNING *",
+    [name]
+  );
+  return addNewDBCategory.rows[0];
+}
