ba76e142507c9a2456842e06cee9d3ec13f3c82f
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-23 13:20:28 +0000

    Create service function that freches menu items from DB. Export it to be used as a helper function for it's respective controller in menuController.js

--- backend/src/services/menuService.js
+++ backend/src/services/menuService.js
@@ -12,3 +12,15 @@ export async function addCategory(name) {
   );
   return addNewDBCategory.rows[0];
 }
+
+
+export async function fetchMenuItems() {
+  const dbStoredMenuItems = await pool.query(`
+  SELECT mi.menu_item_id, mi.name, mc.name AS category, dr.name AS diet
+  FROM menu_items mi
+  JOIN menu_categories mc ON mi.category_id = mc.category_id
+  JOIN dietary_restrictions dr ON mi.diet_id = dr.diet_id
+  ORDER BY mi.menu_item_id
+  `);
+  return dbStoredMenuItems.rows;
+}
