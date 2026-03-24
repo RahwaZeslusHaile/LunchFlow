89247293abd42c21c95fa8aaf27b94d39c62d943
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-24 11:46:19 +0000

    update the helper function for attendance to accomodate the consideration for the attendence date and thus data are fetched for that date rather than all attendance data

--- backend/src/services/attendanceService.js
+++ backend/src/services/attendanceService.js
@@ -1,16 +1,34 @@
 import pool from "../db.js";
 
-export async function fetchAttendance() {
-  const storedAttendanceData = await pool.query(`
-  SELECT
-    a.attendance_id,
-    c.name AS class_name,
-    a.session_date,
-    a.trainee_count,
-    a.volunteer_count
-  FROM attendance a
-  JOIN classes c ON a.class_id = c.class_id
-  ORDER BY a.attendance_id  
-  `);
-  return storedAttendanceData.rows;
+export async function fetchAttendance(date) {
+  let query = `
+    SELECT
+      a.attendance_id,
+      c.name AS class_name,
+      a.session_date,
+      a.trainee_count,
+      a.volunteer_count
+    FROM attendance a
+    JOIN classes c ON a.class_id = c.class_id
+  `;
+
+  const values = [];
+
+  if (date) {
+    query += ` WHERE a.session_date = $1`;
+    values.push(date);
+  }
+
+  query += ` ORDER BY a.attendance_id`;
+
+  const storedAttendanceData = await pool.query(query, values);
+
+  const total = storedAttendanceData.rows.reduce(
+    (sum, row) => sum + row.trainee_count + row.volunteer_count,
+    0
+  );
+  return {
+    data: storedAttendanceData.rows,
+    total
+  };
 }
