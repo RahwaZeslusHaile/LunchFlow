443ea733683cf6cf4cd0b7a20af050010eec0ad5
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-24 11:27:00 +0000

    Create helper function to interact directly with DB to fetch classes data

--- backend/src/services/classesService.js
+++ backend/src/services/classesService.js
@@ -1,1 +1,8 @@
 import pool from "../db.js";
+
+export async function fetchClasses() {
+  const storedClassesData = await pool.query(
+    "SELECT * FROM classes ORDER BY class_id"
+  );
+  return storedClassesData.rows;
+}
