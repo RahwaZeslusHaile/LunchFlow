84c93f12f9ea5defd6685ee0b9e626026f26bf8c
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-04-02 21:24:22 +0100

    Create attendance controller function handler for POST request

--- backend/src/controllers/attendanceController.js
+++ backend/src/controllers/attendanceController.js
@@ -1,5 +1,5 @@
 import {
-    fetchAttendance
+    fetchAttendance, insertAttendance
 } from "../services/attendanceService.js";
 
 function sendError(res, err) {
@@ -15,6 +15,22 @@ export async function getAttendance(req, res) {
     const { date } = req.query;
     const result = await fetchAttendance(date);
     return res.json(result);
+  } catch (err) {
+    return sendError(res, err);
+  }
+}
+
+export async function createAttendance(req, res) {
+  try {
+    const { class_id, trainee_count, volunteer_count } = req.body;
+
+    if (!class_id || trainee_count == null || volunteer_count == null) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const newAttendance = await insertAttendance({ class_id, trainee_count, volunteer_count });
+    return res.status(201).json(newAttendance);
+    
   } catch (err) {
     return sendError(res, err);
   }
