59c80cad75cb3a76febd7708e8bef77ba8ccfdee
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-04-12 23:37:58 +0100

    Feat: update the db file for deployment

--- backend/src/db.js
+++ backend/src/db.js
@@ -8,12 +8,13 @@ const { Pool } = pkg;
 const useSsl = process.env.DB_SSL === "true";
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "launch",
-  password: process.env.DB_PASSWORD || "",
-  port: Number(process.env.DB_PORT || 5432),
-  ssl: useSsl ? { rejectUnauthorized: false } : false
+  user: process.env.DB_USER ,
+  host: process.env.DB_HOST ,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  ssl: useSsl ? { rejectUnauthorized: false }
+     : false
 });
 
 pool.on("error", (err) => {
